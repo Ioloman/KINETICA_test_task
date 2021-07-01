4f64d31190aa2068f943a9c8c9ef8053dfb0c419
loloman <kirilldd2@gmail.com>
2021-07-01 19:12:46 +0700

    added post list and post creation;

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,31 +17,87 @@
         @auth
             <div class="row">
                 <div class="my-3 col-12 col-sm-10 col-md-9 col-lg-7 mx-auto p-5 mb-4 rounded-3 bg-light">
-                    <a href="#"><h4><span class="badge bg-primary mb-3">Создать запись</span></h4></a>
-                    <div class="card">
+                    <a type="button" data-bs-toggle="modal" data-bs-target="#textEditorModal"><h4><span class="badge bg-primary mb-3">Создать запись</span></h4></a>
 
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                        <div class="card-footer text-muted clearfix">
-                            <div class="float-md-start">право</div>
-                            <div class="float-md-end">лево</div>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <div class="card-body">
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <div class="modal fade" id="textEditorModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalLabel">Создать пост</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3 row">
+                                        <label class="col-form-label col-2">Название: </label>
+                                        <div class="col-8">
+                                            <input class="form-control" type="text" placeholder="Название поста" id="title" maxlength="255">
+                                        </div>
+                                    </div>
+                                    <textarea name="text" id="textEditor" cols="60" rows="40"></textarea>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" id="editorSend">Опубликовать</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+                    @yield('posts')
 
                 </div>
             </div>
         @endauth
     </div>
+@endsection
+
+@section('javascript')
+    <script src="{{ URL::to('js/packages/tinymce/tinymce.min.js') }}"></script>
+    <script>
+        $(document).ready(() => {
+            const editor_config = {
+                path_absolute: "{{ URL::to('/') }}/",
+                selector: "textarea#textEditor",
+                language: "ru",
+                height: 400,
+                plugins: [
+                    "advlist autolink lists link charmap print hr anchor pagebreak",
+                    "searchreplace visualblocks visualchars",
+                    "insertdatetime nonbreaking save contextmenu directionality",
+                    "emoticons template paste textcolor colorpicker textpattern"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+                relative_urls: false
+            };
+            tinyMCE.init(editor_config);
+        })
+    </script>
+    <script>
+        $(document).ready(() => {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('#editorSend').on('click', function () {
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ route('post.create') }}',
+                    data: {title: $('#title').val(), text: tinyMCE.get('textEditor').getContent()}
+                }).done((data, textStatus, jqXHR) => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        const card = $(data);
+                        card.hide();
+                        $('div.card:first').before(card);
+                        card.slideDown(400);
+                    }
+                }).fail((jqXHR, textStatus, error) => {
+                    alert(`${textStatus} - ${error}`)
+                });
+                bootstrap.Modal.getInstance(document.getElementById('textEditorModal')).hide();
+            });
+        })
+    </script>
 @endsection
